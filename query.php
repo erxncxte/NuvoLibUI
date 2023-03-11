6658314e2ae409a5476748d7f2ae3849b992a52f
Erin Cate <77478941+erxncxte@users.noreply.github.com>
2023-03-11 15:46:29 +1100

    Adding more buttons to UI

---
+++ query.php
@@ -0,0 +1,14 @@
+<?php
+    // Connect to the database
+    $servername= "";
+    $username= "";
+    $password= "";
+
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+?>
